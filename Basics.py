b6a4ceb7a06b0697212e36d70464681aa38ec16d
NickersF <pdxpunk85@gmail.com>
2014-05-08 00:44:47 -0700

    Test

--- Basics.py
+++ Basics.py
@@ -1,3 +1,18 @@
 __author__ = 'Nick'
-name = "nick"
-print name
+
+# variables and lists to use
+firstnames = ['Lizlo','Nicklo']
+lastnames = [' Pink',' Brown']
+bothnames = firstnames + lastnames
+name = "Nicholas Fazzolari"
+
+# basic if/else statement checking a condition of the variable names.
+if name is not "nick":
+    print "Name is not nick!"
+else:
+    print "Name stored is %s" %name
+
+# adding the first names and last names together from the respective lists
+print bothnames
+print firstnames[1] + lastnames[1]
+print firstnames[0] + lastnames[0]
